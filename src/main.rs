2d174b8f5148c2d348fcd8df176579fdf6225eb2
Josh Jones <ohblonddev@gmail.com>
2022-02-03 17:29:26 +0000

    Add a demo status endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::http::StatusCode;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/admin/status", get(handler));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", address);
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> StatusCode {
+    StatusCode::OK
 }
