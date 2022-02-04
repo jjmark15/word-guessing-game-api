1e2fd30a2b3106d45adadd3a9efee0f86f52a6db
Josh Jones <ohblonddev@gmail.com>
2022-02-04 00:20:40 +0000

    Log listen address as INFO

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
 
     let address = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 3030);
     let listener = TcpListener::bind(address).unwrap();
-    tracing::debug!("server listening on {}", address);
+    tracing::info!("server listening on {}", address);
 
     let app = App::new();
     app.run(listener).await
