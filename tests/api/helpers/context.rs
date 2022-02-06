2aac5afce4428638fce5dfce0d01fb5fa1c022a3
Josh Jones <ohblonddev@gmail.com>
2022-02-06 14:20:13 +0000

    Run server separately from tests

--- tests/api/helpers/context.rs
+++ tests/api/helpers/context.rs
@@ -1,9 +1,6 @@
-use std::net::{IpAddr, Ipv6Addr, SocketAddr, TcpListener};
-
-use tokio::task::JoinHandle;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 
 use api_test_client::{Client, HttpClient};
-use word_guessing_game_api::App;
 
 pub(crate) struct TestContext {
     client: Client,
@@ -11,10 +8,7 @@ pub(crate) struct TestContext {
 
 impl TestContext {
     pub(crate) fn new() -> Self {
-        let listener = Self::tcp_listener();
-        let local_address = listener.local_addr().unwrap();
-
-        let _app = Self::app(listener);
+        let local_address = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 3030);
 
         let client = Client::new(HttpClient::new(), local_address);
         TestContext { client }
@@ -22,14 +16,5 @@ impl TestContext {
 
     pub(crate) fn client(&self) -> &Client {
         &self.client
-    }
-
-    fn tcp_listener() -> TcpListener {
-        TcpListener::bind(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 0)).unwrap()
-    }
-
-    fn app(listener: TcpListener) -> JoinHandle<()> {
-        let app = App::new();
-        tokio::spawn(async move { app.run(listener).await })
     }
 }
